1d614aa49d8a17de93330c84a40ed98993e05e41
devyn cyphers <devyn.cyphers@centeva.com>
2021-02-24 09:09:02 -0700

    work on basic server

--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module devync/Merganser
 go 1.15
 
 require (
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/githubnemo/CompileDaemon v1.2.1 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/jackc/pgx/v4 v4.10.1
 	github.com/jmoiron/sqlx v1.3.1
 	github.com/mattn/go-sqlite3 v1.14.6
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/text v0.3.5 // indirect
 )
--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3"
+
+	_ "github.com/jackc/pgx/stdlib"
 )
 
 type chat struct {
-	ID      int    `json:"id" db:"id"`
+	ID      int    `json:"id" db:"chat_id"`
 	Message string `json:"message" db:"message"`
 	IsRead  bool   `db:"is_read"`
 }
@@ -23,6 +23,8 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/chat/{id}", getChatRequest).Methods("GET")
+	router.HandleFunc("/chat", postChatRequest).Methods("POST")
+
 	server := &http.Server{
 		Addr:    ":3000",
 		Handler: router,
@@ -33,26 +35,40 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
+func connectDB() *sqlx.DB {
+	if dbContext == nil {
+		dbContext = sqlx.MustConnect("pgx", "postgres://dev:MerganserDev@localhost:5432/Merganser?sslmode=disable")
+	}
+	return dbContext
+}
+
+var insertChat = `INSERT INTO chat (chat_id, message, is_read) VALUES (:chat_id, :message, :is_read)`
+
 func setupDB() {
-	schema := `CREATE TABLE chat (id integer, message text NULL, is_read boolean);`
-	insertChat := `INSERT INTO chat (id, mesasge, is_read) VALUES (:id, :message, :is_read)`
+	clearSchema := `DROP SCHEMA public CASCADE;
+	CREATE SCHEMA public;
+	GRANT ALL ON SCHEMA public TO dev;
+	GRANT ALL ON SCHEMA public TO public;`
+
+	schema := `CREATE TABLE chat (chat_id serial primary key, message varchar(255), is_read boolean);`
 
 	chats := []chat{
-		{ID: 0, Message: "Chat-1!", IsRead: false},
-		{ID: 1, Message: "Chat-2!", IsRead: false},
-		{ID: 2, Message: "Chat-3!", IsRead: true},
-		{ID: 3, Message: "Chat-4!", IsRead: true},
+		{ID: 1, Message: "Chat-1!", IsRead: false},
+		{ID: 2, Message: "Chat-2!", IsRead: false},
+		{ID: 3, Message: "Chat-3!", IsRead: true},
+		{ID: 4, Message: "Chat-4!", IsRead: true},
 	}
 
-	db := sqlx.MustConnect("sqlite3", ":memory:")
+	db := connectDB()
 
+	db.MustExec(clearSchema)
 	db.MustExec(schema)
 
-	tx := db.MustBegin()
-	tx.NamedExec(insertChat, chats)
-	tx.Commit()
+	_, err := db.NamedExec(insertChat, chats)
 
-	dbContext = db
+	if err != nil {
+		panic(err)
+	}
 }
 
 func writeResponse(w http.ResponseWriter, o interface{}) {
@@ -68,15 +84,40 @@ func writeResponse(w http.ResponseWriter, o interface{}) {
 
 func getChatRequest(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-	var res *chat
+	id, _ := params["id"]
+	res := chat{}
 
-	dbContext.Get(&res, "SELECT * FROM chat WHERE id=$1", id)
+	err := dbContext.Get(&res, "SELECT * FROM chat WHERE chat_id=$1", id)
 
-	if res == nil {
-		http.Error(w, "No chat found.", http.StatusInternalServerError)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	writeResponse(w, res)
+}
+
+func postChatRequest(w http.ResponseWriter, r *http.Request) {
+	var req chat
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&req); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusInternalServerError)
+	}
+
+	var id int
+
+	rows, err := dbContext.NamedQuery(insertChat+" RETURNING chat_id", req)
+	rows.Scan(&id)
+	defer rows.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	res := chat{}
+
+	dbContext.Get(&res, `SELECT * FORM chat WHERE chat_id=$1`, id)
 
 	writeResponse(w, res)
 }
